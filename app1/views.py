ea8892c15759fe1e82184850ef38f9c3fd2159fa
Jordanizquierdo <169169895+Jordanizquierdo@users.noreply.github.com>
2024-09-10 12:56:27 -0300

    .

--- app1/views.py
+++ app1/views.py
@@ -10,3 +10,13 @@ def inicio(request):
 def electronica(request):
     datos= {"menu":"Electronica","p1":"mac","p2":"iPhone","p3":"Playstation"}
     return render(request, 'app1/pag.html',datos)
+
+
+def juguetes(request):
+    datos= {"menu":"Ropa","p1":"Auto","p2":"Pelota de Futbol","p3":"Figura de Acción"}
+    return render(request, 'app1/pag.html',datos)
+
+
+def ropa(request):
+    datos= {"menu":"Ropa","p1":"Pantalones","p2":"Chaqueta","p3":"Camisa"}
+    return render(request, 'app1/pag.html',datos)
